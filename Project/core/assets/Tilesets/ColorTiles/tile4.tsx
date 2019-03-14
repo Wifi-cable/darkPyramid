a1c51c12c34fddb5758bd230cef726dd6a4329dc
mary <Wifi-cable@gmx.de>
2019-03-14 10:53:39 +0100

    larger map with border

--- Project/core/assets/Tilesets/ColorTiles/tile4.tsx
+++ Project/core/assets/Tilesets/ColorTiles/tile4.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.2" name="tile4" tilewidth="40" tileheight="40" tilecount="144" columns="12">
  <image source="tilea4.png" trans="ff00ff" width="512" height="480"/>
+ <tile id="142">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
 </tileset>
