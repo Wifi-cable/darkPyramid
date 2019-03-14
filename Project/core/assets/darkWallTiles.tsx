8b44561439199275b4b44e94a73aa616f96a9d51
wifi-cable <Wifi-cable@users.noreply.github.com>
2019-03-14 23:43:51 +0100

    5 levels in final design added

---
+++ Project/core/assets/darkWallTiles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="darkWallTiles" tilewidth="40" tileheight="40" tilecount="3" columns="1">
+ <image source="../../../../maps/darkWallTiles.png" trans="ff00ff" width="40" height="120"/>
+ <tile id="0">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+</tileset>
