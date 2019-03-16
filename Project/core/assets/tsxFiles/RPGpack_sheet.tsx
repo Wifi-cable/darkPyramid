79326bff5d6ec1c3646eb3779a7caf4337705ece
Phoenix2707 <37815638+Phoenix2707@users.noreply.github.com>
2019-03-16 17:12:22 +0100

    Add files via upload

---
+++ Project/core/assets/tsxFiles/RPGpack_sheet.tsx
@@ -0,0 +1,15 @@
+<;?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="RPGpack_sheet" tilewidth="40" tileheight="40" tilecount="640"
+         columns="32">
+    <image source="../../../../tilemaps/RPGpack_sheet.png" trans="ff00ff" width="1280" height="832"/>
+    <tile id="501">
+        <properties>
+            <property name="exit" value=""/>
+        </properties>
+    </tile>
+    <tile id="634">
+        <properties>
+            <property name="exit" value=""/>
+        </properties>
+    </tile>
+</tileset>;
---
+++ Project/core/assets/tsxFiles/sandTiles.tsx
@@ -0,0 +1,4 @@
+
+<tileset version="1.2" tiledversion="1.2.2" name="sandTiles" tilewidth="40" tileheight="40" tilecount="10" columns="2">
+    <image source="sandTiles.png" trans="ff00ff" width="80" height="200"/>
+</tileset>;
