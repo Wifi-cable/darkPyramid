302d6bc76e50c92d8b1a2c48ddc5020d836456db
Phoenix2707 <37815638+Phoenix2707@users.noreply.github.com>
2019-03-21 17:35:37 +0100

    Add files via upload

---
+++ Project/core/assets/Tilesets/ColorTiles/lightWall.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="lightWall" tilewidth="1" tileheight="1" tilecount="0" columns="0">
+ <properties>
+  <property name="blocked" value=""/>
+ </properties>
+</tileset>
